6f75fc1121b5e8706bcd7695689e1e3474b6bbbd
Antonio Ojea <aojea@redhat.com>
2020-12-08 01:53:01 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # clusterip-webhook
-validating and mutating webhook to manage  the Kubernetes Services Cluster IPs
+
+validating and mutating webhook to manage the Kubernetes Services Cluster IPs
+
+
+## How to use it
+
+The admin defines the IP Ranges objects with the subnets, the Services ClusterIPs will be assigned
+from this IP ranges.
+
+Currently, it only supports one IPRange per IPFamily with the same value defined in the apiserver
+
+TODO:
+
+1. Move Service IP Range configuration out of the apiserver
+2. Allow to use multiples IPranges
+  - Modify Services to reference IPRange Object
+
+## How it works
+
+When an user creates a Kubernetes Service of Type ClusterIP, NodePort or LoadBalancer, it can
+specify the ClusterIP or the apiserver will allocate one free.
+
+### Create, Update
+
+If the ClusterIP is set by the user, the webhook validates it belongs to the range and is free
+
+If the ClusterIP is not set, the webhook assigns one free from the range
+
+The ClusterIP is inmutable after creation, but when the Service Type changes
+
+### Delete
+
+When a Service is Deleted, the controller will deallocate the ClusterIP assigned from the IPRange
+object once the Service Delete event is received (Not when the Delete request is seen)
+
---
+++ api/v1/iprange_types.go
@@ -0,0 +1,74 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// IPRangeSpec defines the desired state of IPRange
+type IPRangeSpec struct {
+	// +kubebuilder:validation:MaxLength=15
+	// +kubebuilder:validation:MinLength=1
+	Name string `json:"name,omitempty"`
+
+	// Range represent the IP range in CIDR format
+	// i.e. 10.0.0.0/16 or 2001:db2::/64
+	// +kubebuilder:validation:MaxLength=128
+	// +kubebuilder:validation:MinLength=8
+
+	Range string `json:"range,omitempty"`
+	// +optional
+	// Addresses represent the IP addresses of the range and its status.
+	// Each address may be associated to one kubernetes object (i.e. Services)
+	// +listType=set
+	Addresses []string `json:"addresses,omitempty"`
+}
+
+// IPRangeStatus defines the observed state of IPRange
+type IPRangeStatus struct {
+	// Free represent the number of IP addresses that are not allocated in the Range
+	// +optional
+	Free int64 `json:"free,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// IPRange is the Schema for the ipranges API
+type IPRange struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   IPRangeSpec   `json:"spec,omitempty"`
+	Status IPRangeStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// IPRangeList contains a list of IPRange
+type IPRangeList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []IPRange `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&IPRange{}, &IPRangeList{})
+}
